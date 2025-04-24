d6eece7863dccb3f949a65616f0453d1c11362d3
GUO YANKE <hi@yankeguo.com>
2025-04-24 11:13:58 +0800

    feat: complete

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # skopeo-machine
+
 An API server that launches skopeo jobs in Kubernetes
+
+## Usage
+
+```bash
+/skopeo-machine -conf config.json
+```
+
+```plain
+POST /any/path
+
+{
+    "action":"copy",
+    "source":"some/image:111",
+    "target":"registry.mycompany.com/some-image:111"
+}
+```
+
+## Configuration
+
+```json
+{
+  "job": {
+    "namespace": "skopeo",
+    "image": "quay.io/skopeo/stable:latest",
+    "imagePullPolicy": "IfNotPresent",
+    "imagePullSecrets": [
+      {
+        "name": "myregistrykey"
+      }
+    ]
+  },
+  "copy": {
+    "authfileSrc": "myregistrykeysecret-1",
+    "authfileDst": "myregistrykeysecret-2"
+  }
+}
+```
+
+## Credits
+
+GUO YANKE, MIT License
--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"context"
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/creasty/defaults"
+	"github.com/google/uuid"
 	"github.com/yankeguo/rg"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -27,6 +38,60 @@ type Conf struct {
 		AuthfileSrc string `json:"authfileSrc"`
 		AuthfileDst string `json:"authfileDst"`
 	} `json:"copy"`
+}
+
+func ptr[T any](v T) *T {
+	return &v
+}
+
+func createId() string {
+	id := rg.Must(uuid.NewV7())
+	return strings.ReplaceAll(id.String(), "-", "")
+}
+
+func formatSelector(labels map[string]string) string {
+	if len(labels) == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	for k, v := range labels {
+		if sb.Len() > 0 {
+			sb.WriteString(",")
+		}
+		sb.WriteString(fmt.Sprintf("%s=%s", k, v))
+	}
+	return sb.String()
+}
+
+func sha1sum(b []byte) string {
+	h := sha1.New()
+	h.Write(b)
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func standardizeImage(image string) string {
+	if !strings.Contains(image, ":") {
+		image += ":latest"
+	}
+
+	splits := strings.Split(image, "/")
+
+	// single part
+	if len(splits) < 2 {
+		return "docker.io/library/" + image
+	}
+
+	// custom domain
+	if strings.Contains(splits[0], ".") || strings.Contains(splits[0], ":") {
+		// first part is docker.io and only 2 parts
+		if len(splits) == 2 && splits[0] == "docker.io" {
+			return "docker.io/library/" + splits[1]
+		}
+		return image
+	}
+
+	return "docker.io/" + image
 }
 
 func createKubernetesClient() (client *kubernetes.Clientset, err error) {
@@ -63,6 +128,147 @@ func createConf() (conf Conf, err error) {
 
 	rg.Must0(defaults.Set(&conf))
 
+	if conf.Job.Namespace == "" {
+		conf.Job.Namespace = os.Getenv("POD_NAMESPACE")
+	}
+
+	if conf.Job.Namespace == "" {
+		buf, _ = os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+		conf.Job.Namespace = string(buf)
+	}
+
+	if conf.Job.Namespace == "" {
+		err = errors.New("namespace is empty")
+		return
+	}
+
+	return
+}
+
+var (
+	gConf   Conf
+	gClient *kubernetes.Clientset
+	gLocker = &sync.Mutex{}
+)
+
+type copyOptions struct {
+	Source string `json:"source"`
+	Target string `json:"target"`
+}
+
+func doCopy(ctx context.Context, opts copyOptions) (err error) {
+	gLocker.Lock()
+	defer gLocker.Unlock()
+	defer rg.Guard(&err)
+
+	log.Printf("copy from %s to %s", opts.Source, opts.Target)
+
+	opts.Source = standardizeImage(opts.Source)
+	opts.Target = standardizeImage(opts.Target)
+
+	sourceDigest := sha1sum([]byte(opts.Source))
+	targetDigest := sha1sum([]byte(opts.Target))
+
+	jobLabels := map[string]string{
+		"com.yankeguo.skopeo-machine/copy-source": sourceDigest,
+		"com.yankeguo.skopeo-machine/copy-target": targetDigest,
+	}
+
+	existed := rg.Must(gClient.BatchV1().Jobs(gConf.Job.Namespace).List(ctx, metav1.ListOptions{
+		LabelSelector: formatSelector(jobLabels),
+	})).Items
+
+	var existedValid bool
+
+	for _, job := range existed {
+		if job.Status.Active > 0 {
+			existedValid = true
+		} else {
+			if job.Status.CompletionTime != nil {
+				if time.Now().Sub(job.Status.CompletionTime.Time) > time.Hour*24 {
+					rg.Must0(gClient.BatchV1().Jobs(gConf.Job.Namespace).Delete(ctx, job.Name, metav1.DeleteOptions{}))
+				} else {
+					existedValid = true
+				}
+			} else {
+				rg.Must0(gClient.BatchV1().Jobs(gConf.Job.Namespace).Delete(ctx, job.Name, metav1.DeleteOptions{}))
+			}
+		}
+	}
+
+	if existedValid {
+		log.Println("job already exists, skip")
+		return
+	}
+
+	job := &batchv1.Job{}
+	job.ObjectMeta.Name = "skopeo-copy-" + createId()
+	job.ObjectMeta.Namespace = gConf.Job.Namespace
+	job.ObjectMeta.Labels = jobLabels
+	job.Spec.TTLSecondsAfterFinished = ptr[int32](600)
+	job.Spec.Template.ObjectMeta.Labels = jobLabels
+	job.Spec.Template.Spec.RestartPolicy = corev1.RestartPolicyOnFailure
+	job.Spec.Template.Spec.ImagePullSecrets = gConf.Job.ImagePullSecrets
+	if gConf.Copy.AuthfileSrc != "" {
+		job.Spec.Template.Spec.Volumes = []corev1.Volume{
+			{
+				Name: "authfile-src",
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: gConf.Copy.AuthfileSrc,
+					},
+				},
+			},
+		}
+	}
+	if gConf.Copy.AuthfileDst != "" {
+		job.Spec.Template.Spec.Volumes = append(job.Spec.Template.Spec.Volumes, corev1.Volume{
+			Name: "authfile-dst",
+			VolumeSource: corev1.VolumeSource{
+				Secret: &corev1.SecretVolumeSource{
+					SecretName: gConf.Copy.AuthfileDst,
+				},
+			},
+		})
+	}
+	container := corev1.Container{
+		Name:            "skopeo-copy",
+		Image:           gConf.Job.Image,
+		ImagePullPolicy: gConf.Job.ImagePullPolicy,
+	}
+	if gConf.Copy.AuthfileSrc != "" {
+		container.VolumeMounts = []corev1.VolumeMount{
+			{
+				Name:      "authfile-src",
+				MountPath: "/authfile-src",
+			},
+		}
+	}
+	if gConf.Copy.AuthfileDst != "" {
+		container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
+			Name:      "authfile-dst",
+			MountPath: "/authfile-dst",
+		})
+	}
+	args := []string{
+		"skopeo",
+		"copy",
+		"--multi-arch=all",
+	}
+	if gConf.Copy.AuthfileSrc != "" {
+		args = append(args, "--src-authfile=/authfile-src/.dockerconfigjson")
+	}
+	if gConf.Copy.AuthfileDst != "" {
+		args = append(args, "--dest-authfile=/authfile-dst/.dockerconfigjson")
+	}
+	args = append(args, "docker://"+opts.Source, "docker://"+opts.Target)
+
+	container.Args = args
+
+	job.Spec.Template.Spec.Containers = []corev1.Container{container}
+
+	rg.Must(gClient.BatchV1().Jobs(gConf.Job.Namespace).Create(ctx, job, metav1.CreateOptions{}))
+
 	return
 }
 
@@ -77,9 +283,9 @@ func main() {
 	}()
 	defer rg.Guard(&err)
 
-	conf := rg.Must(createConf())
+	gConf = rg.Must(createConf())
 
-	client := rg.Must(createKubernetesClient())
+	gClient = rg.Must(createKubernetesClient())
 
 	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var err error
@@ -103,7 +309,22 @@ func main() {
 		switch data.Action {
 		case "copy":
 			{
+				if data.Source == "" {
+					err = fmt.Errorf("source is empty")
+					return
+				}
+				if data.Target == "" {
+					err = fmt.Errorf("target is empty")
+					return
+				}
+				rg.Must0(doCopy(r.Context(), copyOptions{
+					Source: data.Source,
+					Target: data.Target,
+				}))
 			}
+		default:
+			err = fmt.Errorf("unknown action: %s", data.Action)
 		}
 	}))
+
 }
